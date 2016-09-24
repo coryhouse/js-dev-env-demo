54b2936b47fbdfe8f4001bdd23fc50902f6f4e3c
Cory House <housecor@gmail.com>
2016-09-24 12:26:30 -0500

    end of module 5

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
