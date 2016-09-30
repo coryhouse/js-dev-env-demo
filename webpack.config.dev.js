92a8ed105455eff241f50ae870a8094b9321e468
Cory House <housecor@gmail.com>
2016-09-29 23:14:13 -0500

    Add css loader and style loader - should have been in package.json at beginning.

---
+++ webpack.config.dev.js
@@ -0,0 +1,27 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+};
