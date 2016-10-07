1d9ace0d5cfdd292e54a5dcefc4b8aaf6c70b7f2
Cory House <housecor@gmail.com>
2016-10-06 21:49:52 -0500

    module 9 work in progress

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello World!");
+      done();
+      window.close();
+    });
+  })
+})
